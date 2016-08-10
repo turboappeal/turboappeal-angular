de1646ea8aa40aceb3756cf356caedd08aeb3d44
joan <joan@turboappeal.com>
2016-08-10 17:22:28 -0300

    Deleted typings

--- typings/index.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="globals/angular/index.d.ts" />
-/// <reference path="globals/jquery/index.d.ts" />
-/// <reference path="globals/jssha/index.d.ts" />
-/// <reference path="globals/moment-node/index.d.ts" />
-/// <reference path="globals/moment/index.d.ts" />
