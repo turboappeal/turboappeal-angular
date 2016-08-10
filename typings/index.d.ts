53a3a0410e045029299019ffffd55abcb486a606
joan <joan@turboappeal.com>
2016-08-10 17:21:27 -0300

    updated repo

---
+++ typings/index.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="globals/angular/index.d.ts" />
+/// <reference path="globals/jquery/index.d.ts" />
+/// <reference path="globals/jssha/index.d.ts" />
+/// <reference path="globals/moment-node/index.d.ts" />
+/// <reference path="globals/moment/index.d.ts" />
